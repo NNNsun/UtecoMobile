ebb7f5ff820396ce40fac5c24077c7a2792fd050
sun <niketjssun31@gmail.com>
2022-11-01 22:44:42 +0900

    곱셈을 위해 Iterable객체로 mul()함수를 정의

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -13,6 +13,13 @@ fun calculate(num: Int): Int {
     return if (a > b) a else b
 }
 fun Iterable<Int>.mul(): Int {
+    var mul = 1
+    for (element in this) {
+
+        if(element==0) return 0
+        mul *= element
+    }
+    return mul
 
 }
 
