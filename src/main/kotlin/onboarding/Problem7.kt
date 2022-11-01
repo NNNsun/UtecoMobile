98e4441513640b2612b9b12dcd0449e798c00742
sun <niketjssun31@gmail.com>
2022-11-01 23:35:31 +0900

    user의 친구들과 user를 set에 저장, 중복값을 제거하기 위해 set을 사용한다

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -5,5 +5,19 @@ fun solution7(
     friends: List<List<String>>,
     visitors: List<String>
 ): List<String> {
-    TODO("프로그램 구현")
+    var dupList=friends.toMutableList()
+    var map = HashMap<String, Int>()
+    var set =HashSet<String>()
+
+    for (i in 0 until dupList.size) {
+        if (dupList[i][0] == user) {
+            set.add(dupList[i][1])
+
+        } else if (dupList[i][1] == user) {
+            set.add(dupList[i][0])
+
+        }
+    }
+    set.add(user)
+    return list
 }
