190789127569ace3d11e511b8839e3999c31cfcd
sun <niketjssun31@gmail.com>
2022-11-01 23:13:05 +0900

    아스키코드 알파벳 범위안에서만 체크한다

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    var key=' '
+    var chaBuf =word.toCharArray()
+    var len = chaBuf.size
+    for(i in 0 until len){
+        key= chaBuf[i]
+        if(key in 'a'..'z') {
+            chaBuf[i]='z'-(chaBuf[i]-'a')
+        }
+
+    }
+    return String(chaBuf)
 }
