2536bcfd5b27d11aa38295fb84604df2bdda4443
sun <niketjssun31@gmail.com>
2022-11-01 23:37:58 +0900

    friends에서 set에 저장된 값을 제외한 사람을 map의 key로 넣고 value엔 +10을 해준다

--- src/main/kotlin/onboarding/Problem7.kt
+++ src/main/kotlin/onboarding/Problem7.kt
@@ -19,5 +19,20 @@ fun solution7(
         }
     }
     set.add(user)
+    var overlapMan1 = ""
+    var overlapMan2 = ""
+    for (i in 0 until dupList.size) {
+        overlapMan1=dupList[i][0]
+        overlapMan2=dupList[i][1]
+        if(!set.contains(overlapMan1)) {
+            map[overlapMan1] = map.getOrDefault(overlapMan1, 0) + 10
+            continue
+        }
+        else if (!set.contains(overlapMan2)) {
+            map[overlapMan2] = map.getOrDefault(overlapMan2, 0) + 10
+            continue
+        }
+    }
+
     return list
 }
