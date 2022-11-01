fe7cfa14e5da1b930cc97c54f2aad8e1cae3dfea
sun <niketjssun31@gmail.com>
2022-11-01 22:56:37 +0900

    최근 stack에 담긴 글자와 받을 글자를 비교해서 같을땐 최근 stack의 최상단 글자를 pop하고 마지막엔 stack에 쌓인 글자를 모두 이어서 반환한다

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -6,4 +6,20 @@ fun solution2(cryptogram: String) :String{
     return refine(word)
 }
 
-
+fun refine(list: ArrayList<String>): String {
+    var stack = Stack<String>()
+    var len = list.size
+    var str = ""
+    for (i in 0 until len) {
+        if (stack.empty()) {
+            stack.push(list[i])
+        } else
+            if (stack.peek() == list[i]) stack.pop()
+            else stack.push(list[i])
+        println(stack)
+    }
+    for(i in 0 until stack.size){
+        str+= stack[i]
+    }
+    return str
+}
