ee141a774a3bd363affc5aeb9aa2a097d1f76b7f
sun <niketjssun31@gmail.com>
2022-11-01 22:48:26 +0900

    1. 예외처리  1)책은 오른쪽 page 숫자가 왼쪽 page 숫자보다 크다는 것을 예외처리의 기준으로 둔다  2)쪽이 3개이상이면 예외처리한다  -> -1로 반환한다

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,16 @@
 package onboarding
+
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
+    if (pobi.size != 2 || crong.size != 2) {
+        return -1;
+    }
+
+    return if (pobi[1] - pobi[0] == 1 && crong[1] - crong[0] == 1) {
+        val pbNum = choice(calculate(pobi[0]), calculate(pobi[1]))
+        val crNum = choice(calculate(crong[0]), calculate(crong[1]))
+        win(pbNum, crNum)
+
+    } else -1
 
 }
 
@@ -12,11 +23,12 @@ fun calculate(num: Int): Int {
     }.mul()
     return if (a > b) a else b
 }
+
 fun Iterable<Int>.mul(): Int {
     var mul = 1
     for (element in this) {
 
-        if(element==0) return 0
+        if (element == 0) return 0
         mul *= element
     }
     return mul
@@ -24,9 +36,8 @@ fun Iterable<Int>.mul(): Int {
 }
 
 
-
-fun choice(num1: Int,num2: Int):Int{
-    return if(num1>num2) num1 else num2
+fun choice(num1: Int, num2: Int): Int {
+    return if (num1 > num2) num1 else num2
 
 }
 
