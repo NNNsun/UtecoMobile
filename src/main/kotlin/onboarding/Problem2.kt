a89915a02d6b5b28d171b7765d15117965e76d56
sun <niketjssun31@gmail.com>
2022-11-01 22:52:05 +0900

    string을 한글자씩 받는 변수를 만들어 cryptogram과 비교할수있게 만든다

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,9 @@
 package onboarding
+import java.util.*
 
-fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+fun solution2(cryptogram: String) :String{
+    var word = cryptogram.chunked(1).toMutableList() as ArrayList<String>
+    return refine(word)
 }
+
+
