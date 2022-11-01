c55d9b475b30829ad2fe28ce0f4825c6c748f934
sun <niketjssun31@gmail.com>
2022-11-01 23:29:15 +0900

    별명을 최대 2개 글자로 1씩 늘려가며 순차적으로 쪼갠다

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,17 @@
 package onboarding
-
-fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+fun solution6(forms: List<List<String>>) : List<String>{
+    var map = HashMap<String, Int>()
+    var result = mutableListOf<String>()
+    var divWord = ""
+    var comparison = ""
+    var len = forms.size //4
+    for (i in 0 until len) { //0..3
+        divWord = forms[i][1]
+        for (i in 1 until divWord.length) {
+            comparison = divWord[i - 1] + divWord[i].toString()
+        }
+    }
+    return result.sorted()
 }
+
+
