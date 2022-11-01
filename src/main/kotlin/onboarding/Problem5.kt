36ae2defa58c6f305ac3db343d207f0d11b5f38a
sun <niketjssun31@gmail.com>
2022-11-01 23:21:53 +0900

    단위가 줄어드는 폭이 5와 2가 두개만 반복되므로 true false로 단위를 줄여나간다

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,23 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var unit = 50000
+    var balance = money
+    var turn = true
+    val list = mutableListOf<Int>()
+    val totalUnit=9
+    while (balance > 0) {
+        list.add(balance / unit)
+        balance %= unit
+
+        unit /= if (turn) {
+            5
+        } else if (unit == 10) {
+            list.add(balance % unit)
+            break
+        } else 2
+        turn = !turn
+    }
+
+    return list
 }
