a58b651924f68874d22df9937193abc8ef2273df
sun <niketjssun31@gmail.com>
2022-11-01 22:43:13 +0900

    어떤 값의 자릿 수들의 합과 곱을 계산하고 비교 후 더 큰 값을 반환

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -4,7 +4,15 @@ fun solution1(pobi: List<Int>, crong: List<Int>): Int {
 }
 
 fun calculate(num: Int): Int {
-
+    val a = num.toString().map {
+        it.code - '0'.code
+    }.sum()
+    val b = num.toString().map {
+        it.code - '0'.code
+    }.mul()
+    return if (a > b) a else b
+}
+fun Iterable<Int>.mul(): Int {
 
 }
 
