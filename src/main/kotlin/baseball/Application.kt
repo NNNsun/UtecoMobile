d42369d38d128d3aaed7476dfa2f802b541b3ff0
sun <niketjssun31@gmail.com>
2022-11-08 14:26:27 +0900

    feat: 컴퓨터 측 3자리 수 랜덤 생성
    중복되는 숫자가 없는 3자리 수를 랜덤으로 생성합니다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+private fun makeComNumber(): List<String> {
+    val comNumber: MutableList<String>
+    val randomNumber = mutableSetOf<String>()
+    while (randomNumber.size < 3) {
+        randomNumber.add(Randoms.pickNumberInRange(1, 9).toString())
+    }
+    comNumber = randomNumber.toMutableList()
+    return comNumber
 }
