d02c67d0f9c4a5a82ceb2da51e76a1911f5dd9da
sun <niketjssun31@gmail.com>
2022-11-08 22:31:15 +0900

    test: 세 자릿수 초과 예외 테스트로 테스트명 변경
    예외 테스트를 세자릿수 초과 예외 테스트로 변경합니다.
    세자릿수가 넘는 입력값을 테스트합니다.

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -21,7 +21,7 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `예외 테스트`() {
+    fun `세 자릿수 초과 예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
         }
