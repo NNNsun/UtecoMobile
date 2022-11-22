4f751274928163ef8fd572a908ba1af872f7eb59
sun <niketjssun31@gmail.com>
2022-11-22 20:31:58 +0900

    feat: 게임 안내 문구 게임 진행 안내와 관련된 문구입니다

--- src/main/kotlin/bridge/Comment.kt
+++ src/main/kotlin/bridge/Comment.kt
@@ -1,6 +1,12 @@
 package bridge
 
 class Comment {
+    enum class GameComment(val message: String) {
+        START("다리 건너기 게임을 시작합니다.\n"),
+        WIN("게임 성공 여부: "),
+        RESULT("최종 게임 결과"),
+        COUNT("총 시도한 횟수: ");
+    }
 
 }
 
