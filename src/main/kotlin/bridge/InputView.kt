7e5e690cced9445f498f0c1092c713edfef68975
sun <niketjssun31@gmail.com>
2022-11-22 21:53:07 +0900

    feat: U/D를 입력 받는 함수 이동 할 방향을 입력 받고 U 또는 D인지 확인합니다

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -11,4 +11,16 @@ class InputView {
         }
         return size.toInt()
     }
+
+    fun readMoving(): String {
+        println(Comment.InputComment.CHOICE.message)
+        val input = Console.readLine()
+        if (!(Exception(input).checkOverLength())) return readMoving()
+        if (!(Exception(input).checkLessLength())) return readMoving()
+        if (!(Exception(input).isString())) return readMoving()
+        if (!(Exception(input).checkUpperCase())) return readMoving()
+        if (!(Exception(input).checkDirection())) return readMoving()
+        return input
+    }
+
 }
