93e1753303c5e829818255d735d41afe3272e02b
sun <niketjssun31@gmail.com>
2022-11-22 20:12:08 +0900

    feat: 에러 메세지 서두 문구 출력 에러 메세지 앞에 [ERROR]를 붙여줍니다

--- src/main/kotlin/bridge/Exception.kt
+++ src/main/kotlin/bridge/Exception.kt
@@ -1,6 +1,11 @@
 package bridge
 
 class Exception(var input: String) {
+    private fun printError(message: String) {
+        val errorIntro = "[ERROR] "
+        println(errorIntro + message)
+    }
+
     enum class BridgeSizeErrorState(val message: String) {
         INTEGER("다리 길이에 정수가 아닌 수는 포함 할 수 없습니다."),
         RANGE("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
