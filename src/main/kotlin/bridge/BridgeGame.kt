a660289e49691a3737c116482098d7eecf92ee1d
sun <niketjssun31@gmail.com>
2022-11-22 22:28:50 +0900

    feat: 비교 값 저장 현재 유저의 입력값과 정답여부를 각각의 리스트에 저장합니다

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,9 +4,21 @@ class BridgeGame {
     var countTry = 1
     var myPicks = mutableListOf<String>()
     var bridgeAnswer = mutableListOf<Boolean>()
+
+    private fun addAnswer(bridgeSquare: String, guess: Boolean) {
+        when (guess) {
+            true -> {
+                myPicks.add(bridgeSquare);bridgeAnswer.add(guess)
+            }
+            false -> {
+                myPicks.add(bridgeSquare);bridgeAnswer.add(guess)
+            }
+        }
+    }
+
     fun move(currentBridge: List<String>) {
         when {
-            currentBridge.isNotEmpty()->{}
+            currentBridge.isNotEmpty() -> {}
             currentBridge.isEmpty() -> {}
         }
     }
