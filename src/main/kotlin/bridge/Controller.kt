e2a606035521cba9395071866d4717f0a40dea3b
sun <niketjssun31@gmail.com>
2022-11-22 22:36:16 +0900

    feat: R/Q 입력 안내 게임 진행 여부에대해 입력을 요구하는 안내 함수입니다

--- src/main/kotlin/bridge/Controller.kt
+++ src/main/kotlin/bridge/Controller.kt
@@ -4,4 +4,7 @@ class Controller {
     fun writeMoving(): String {
         return InputView().readMoving()
     }
+    fun writeGameCommand(): String {
+        return InputView().readGameCommand()
+    }
 }
