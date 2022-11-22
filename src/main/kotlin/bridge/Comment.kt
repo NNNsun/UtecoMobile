c53d9bc18462568c31ebed573811b6d3f924d0c3
sun <niketjssun31@gmail.com>
2022-11-22 20:32:36 +0900

    feat: 입력 안내 문구 게임 진행 시 유저에게 입력을 요구할 때 사용되는 문구입니다

--- src/main/kotlin/bridge/Comment.kt
+++ src/main/kotlin/bridge/Comment.kt
@@ -8,6 +8,11 @@ class Comment {
         COUNT("총 시도한 횟수: ");
     }
 
+    enum class InputComment(val message: String) {
+        RANGE("다리의 길이를 입력해주세요."),
+        CHOICE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+        OVER("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
 }
 
 
