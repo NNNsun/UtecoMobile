2ac939aa36b6bc1eee6867bcd296d48b9eeae50c
sun <niketjssun31@gmail.com>
2022-11-22 22:26:41 +0900

    feat: 이동 여부 확인 유저가 맞을 때와 틀렸을 때에 맞게 게임을 진행시킵니다

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -4,7 +4,12 @@ class BridgeGame {
     var countTry = 1
     var myPicks = mutableListOf<String>()
     var bridgeAnswer = mutableListOf<Boolean>()
-    fun move() {}
+    fun move(currentBridge: List<String>) {
+        when {
+            currentBridge.isNotEmpty()->{}
+            currentBridge.isEmpty() -> {}
+        }
+    }
 
     fun retry() {}
 }
