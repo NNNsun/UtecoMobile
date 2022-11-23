72281c9f04b23f10a1f23905b5df9965b36b2930
sun <niketjssun31@gmail.com>
2022-11-23 15:22:33 +0900

    refactor: 기능 소개 이해를 돕기위해 Comment 모든 기능에 주석을 추가했습다

--- src/main/kotlin/bridge/Comment.kt
+++ src/main/kotlin/bridge/Comment.kt
@@ -1,13 +1,14 @@
 package bridge
 
 class Comment {
+    //게임 안내 멘트
     enum class GameComment(val message: String) {
         START("다리 건너기 게임을 시작합니다.\n"),
         WIN("게임 성공 여부: "),
         RESULT("최종 게임 결과"),
         COUNT("총 시도한 횟수: ");
     }
-
+    //입력 요구 멘트
     enum class InputComment(val message: String) {
         RANGE("다리의 길이를 입력해주세요."),
         CHOICE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
