cc4ef6f0aeb01d7adf532ea30814d9e0e47d2cab
sun <niketjssun31@gmail.com>
2022-11-23 16:23:56 +0900

    refactor: 기능 소개 이해를 돕기위해 BridgeMaker의 모든 기능에 주석을 추가했습다

--- src/main/kotlin/bridge/BridgeMaker.kt
+++ src/main/kotlin/bridge/BridgeMaker.kt
@@ -1,6 +1,7 @@
 package bridge
 
 class BridgeMaker(private val bridgeNumberGenerator: BridgeNumberGenerator) {
+    // 1.랜덤 값 생성 (0,1)->(D,U) 2.list 저장
     fun makeBridge(size: Int): List<String> {
         val bridge = mutableListOf<String>()
         for (i in 0 until size) {
